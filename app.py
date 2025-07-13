77a9815c503780c5df4d8a44c5d603e2f8eba9f8
Michael Irlam <michaelirlam@hotmail.co.uk>
2025-07-13 20:36:06 +0100

    added route for reviews

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from restaurants.api.restaurant_resource import RestaurantResource
+from restaurants.api.review_resource import ReviewResource
 
 # Initialise the Flask app
 app = Flask(__name__)
 api = Api(app) # wraps the app with the flask_restful Api class
 
 api.add_resource(RestaurantResource, "/restaurants", "/restaurants/<int:restaurant_id>")
+api.add_resource(ReviewResource, "/restaurants/<int:restaurant_id>/reviews", "/restaurants/<int:restaurant_id>/reviews/<int:review_id")
 
 @app.route("/")
 def hello():
